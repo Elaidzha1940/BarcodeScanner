53686d257d35f380ed01d8c04d67451c3428dbf7
Elaidzha1940 <eli080293@gmail.com>
2024-02-05 00:35:03 +0300

    BarcodeScanner

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: BarcodeScanner
+//  File: BarcodeScannerApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 05.02.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: BarcodeScanner
+//  File: BarcodeScannerView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 05.02.2024
+//
+//  */
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        
+        VStack {
+         
+        }
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
